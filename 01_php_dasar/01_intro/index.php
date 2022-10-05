2a0c1b41ac8cc314568d4285d04309c3d3bd5159
suntree99 <suntree99@ymail.com>
2022-10-06 05:35:26 +0700

    upload jwt

--- 01_php_dasar/01_intro/index.php
+++ 01_php_dasar/01_intro/index.php
@@ -11,7 +11,7 @@
     ?>
     
     <!-- 
-    Client Side Scripting (HTML, CSS, Javascript) - pemrosesan dilakukan di komputer client, file bisa disimpan dimana saja dan dibukan dengan mudah
+    Client Side Scripting (HTML, CSS, Javascript) - pemrosesan dilakukan di komputer client, file bisa disimpan dimana saja dan dibuka dengan mudah
     Server Side Scripting (PHP, ASP, Ruby, Python) - pemrosesan dilakukan di komputer server, file harus ditempatkan di tempat khusus dan dibuka dengan menjalankan server (Apache, dll)
 
     Web Browser(Chrome) <-> Web Server (Apache) <-> Script (PHP) <-> Database (MySQL)
@@ -19,6 +19,7 @@
     Cara menajalankan file php:
     - Pastikan file php ada di dalam folder htdocs dari XAMPP
     - Buka program XAMPP dan aktifkan Apache
+    - Pada program XAMPP aktifkan MySQL (jika menggunakan database)
     - Buka browser dan arahkan ke localhost (htdocs)
     - Browser akan membaca file index.php (jika ada) di folder htdocs, jika tidak akan menamilkan isi folder
     - Direktori akan diarahkan ke folder dashboard dan memangil index.php / index.html (jika ada)
